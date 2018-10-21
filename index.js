7139648999478a9afb53c86ae775b97b50066599
Andrew Kim <akim618@gmail.com>
2018-10-21 21:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-var number = 10
+var a = 1
+var b = 2
 
-function add5() {
-  number += 5
+it(a + b){
+  return a + b
 }
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-
-console.log(number) // 3.333333333335
-
-add5()
-
-console.log(number)
