2574523c9528b600ee708ad2c24ff6499905ddf0
Andrew Kim <akim618@gmail.com>
2018-10-21 21:50:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var a = 1
-var b = 2
+ar number = 10
 
-console.log(a + b)
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) 
